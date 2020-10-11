03366c58ee4aed035907fa4f25d77d0d2445feb4
bittenhelen83 <65275228+bittenhelen83@users.noreply.github.com>
2020-10-11 19:52:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+//Question 1
+var petArray = ["cat", "cow", "dog"];
+
+//Question 2
+console.log(petArray[1]);
+
+//Question 3
+petArray.push("sheep");
+
+//Question 4
+console.log(petArray.length);
+
+//Question 5
+var catObject = {
+    name: "Sam",
+    colour: "black",
+    age: 6
+}
+
+//Question 6
+console.log(catObject.age);
+
+//Question 7
+var catArray = [
+    {
+        name: "Sam",
+        colour: "black",
+        age: 6
+    }
+];
+
+//Question 8
+for(var i=0; i < catArray.length; i++) {
+    console.log(catArray[i]);
+}
+
+//Question 9
+function logToConsole(Hi) {
+    console.log(Hi);
+}
+
+//Question 10
+?
